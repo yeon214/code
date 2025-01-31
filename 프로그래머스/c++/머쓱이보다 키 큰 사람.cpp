b460d32b7d8ed3e12e938e6aa19185c1c24c05fc
yeon214 <sunyeon1115@gmail.com>
2025-02-01 00:20:57 +0900

    .

--- 프로그래머스/c++/머쓱이보다 키 큰 사람.cpp
+++ 프로그래머스/c++/머쓱이보다 키 큰 사람.cpp
@@ -1,20 +1,20 @@
-//#include <string>
-//#include <vector>
-//#include <algorithm>
-//using namespace std;
-//
-//int solution(vector<int> array, int height) {
-//    int answer = 0, resultArr[100], a, index;
-//    for (a = 0; a < array.size(); a++) {
-//        resultArr[a] = array[a]; //복사
-//    }
-//    sort(resultArr, resultArr + array.size());
-//    for (a = 0; a < array.size(); a++) {
-//        if (height < array[a]) {
-//            index = a;
-//            break;
-//        }
-//    }
-//    answer = array.size() - index;
-//    return answer;
-//}
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0, resultArr[100], a, index;
+    for (a = 0; a < array.size(); a++) {
+        resultArr[a] = array[a]; //복사
+    }
+    sort(resultArr, resultArr + array.size());
+    for (a = 0; a < array.size(); a++) {
+        if (height < array[a]) {
+            index = a;
+            break;
+        }
+    }
+    answer = array.size() - index;
+    return answer;
+}
--- 프로그래머스/c++/짝수의 합.cpp
+++ 프로그래머스/c++/짝수의 합.cpp
@@ -1,12 +1,12 @@
-﻿//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//
-//int solution(int n) {
-//    int answer = 0, a;
-//    for (a = 0; a <= n; a++) {
-//        if (a % 2 == 0) answer += a;
-//    }
-//    return answer;
-//}
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0, a;
+    for (a = 0; a <= n; a++) {
+        if (a % 2 == 0) answer += a;
+    }
+    return answer;
+}
