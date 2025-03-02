9946ea79a79378fe47e8b7af7ba2f3995c0854e7
yeon214 <sunyeon1115@gmail.com>
2025-03-03 02:05:52 +0900

    .

---
+++ 백준/c++/24263.cpp
@@ -0,0 +1,9 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int main(void) {
+	int n;
+	cin >> n;
+	cout << n << "\n" << "1";
+	return 0;
+}
---
+++ 백준/c++/24265.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int main(void) {
+	long long n;
+	cin >> n;
+	cout << n * (n - 1) / 2 << "\n" << "2";
+	return 0;
+}
