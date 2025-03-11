016988ed7f395475ee9e72b8260d493926b33bc3
yeon214 <sunyeon1115@gmail.com>
2025-03-11 19:33:01 +0900

    .

---
+++ 백준/c++/1920.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int main(void) {
+	int n, list[100000], m, target;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &list[i]);
+	}
+	sort(list, list + n);
+	scanf("%d", &m);
+	while (m--) {
+		scanf("%d", &target);
+		printf("%d\n", binary_search(list, list + n, target));
+	}
+	return 0;
+}
