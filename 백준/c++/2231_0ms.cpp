76bcbc1d3bec479f7603294b0f323bcb13a2901e
yeon214 <sunyeon1115@gmail.com>
2025-03-26 16:53:49 +0900

    .

---
+++ 백준/c++/2231_0ms.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n, sum = 0, change = 0;
+    scanf("%d", &n);
+    for (int i = n - 54; i < n; i++) {
+        sum = change = i;
+        while (change > 0) {
+            sum += change % 10;
+            change /= 10;
+        }
+        if (sum == n) {
+            printf("%d", i);
+            return 0;
+        }
+    }
+    printf("0");
+    return 0;
+}
