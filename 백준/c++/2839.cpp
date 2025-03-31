006cd442582d62310807924668f224390d9726f1
yeon214 <sunyeon1115@gmail.com>
2025-03-31 19:04:06 +0900

    .

---
+++ 백준/c++/2839.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = n / 5; i >= 0; i--) {
+		if ((n - i * 5) % 3 == 0) {
+			printf("%d", i + (n - i * 5) / 3);
+			return 0;
+		}
+	}
+	printf("%d", -1);
+}
