058ef0a30ec7e2e571793b173720175b07a98937
yeon214 <sunyeon1115@gmail.com>
2025-03-31 21:05:19 +0900

    .

---
+++ 백준/c++/19532.cpp
@@ -0,0 +1,9 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(void) {
+	int a, b, c, d, e, f;
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+	printf("%d %d", (c * e - b * f )/( a * e - b * d),( c * d - a * f )/( b * d - a * e));
+}
