353cc96b0ba659f27529b500a9432f783bf40277
yeon214 <sunyeon1115@gmail.com>
2025-04-02 18:50:33 +0900

    .

---
+++ ascode/c++/1143.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	printf("%.0lf", pow(2, n));
+}
